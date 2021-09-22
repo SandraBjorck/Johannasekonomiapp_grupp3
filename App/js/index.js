d5948b152d076764c03d0efbf76c67ddba97c832
Sime Maric <simema2000@gmail.com>
2021-09-22 15:09:33 +0200

    Continue working on popup window

--- App/js/index.js
+++ App/js/index.js
@@ -1,20 +1,24 @@
 // Variables
 const modal = document.getElementById('reggaModal');
 const signupBtn = document.getElementById('signup-modal')
+const modalContent = document.querySelector('.reggaModal-content')
 
 //Eventlisteners
 signupBtn.addEventListener('click', openModal);
-window.addEventListener('click', clickOutside);
+window.addEventListener('click', closeModal);
 
 
 //Function to open modal
 function openModal() {
     modal.style.display = 'block';
+    document.body.classList.add('noscroll');
 }
 
 //Function to close modal
-function clickOutside(e) {
-    if (e.target === modal) {
+function closeModal(e) {
+    if (e.target === modalContent) {
+        document.body.classList.remove('noscroll')
         modal.style.display = 'none';
     }
 }
+
